25429e7f022cfd47c361163ddda65c11d366b3c0
Nick Qantaria <skott56@outlook.com>
2021-03-01 14:54:37 +0400

    added IoC

---
+++ eMobile.IoC/IoCService.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Common.Bus.CQRS;
+using Common.Bus.RabbitMQ;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eMobile.IoC
+{
+    public class IoCService
+    {
+        public static void RegisterServices(ContainerBuilder builder, IServiceCollection services)
+        {
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(scopeFactory);
+            });
+
+            services.AddTransient<ICommandBusAsync, CommandBusAsync>();
+        }
+    }
+}
